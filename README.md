b27d5317a5b72eab373d5ff9ade68f1a36d4eb97
remzi.dalyan <rdalyan16@posta.pau.edu.tr>
2023-07-29 19:09:21 +0300

    Library project initial commit and Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# tr-tax-number-verification
+# Tr Tax Number Faker
+
+<div style="text-align: center">
+<a href="https://packagist.org/packages/netkod-bilisim/tr-tax-number-verification" rel="nofollow">
+    <img src="https://img.shields.io/packagist/v/netkod-bilisim/tr-tax-number-verification" alt="Latest Stable Version">
+</a>
+
+<a href="https://packagist.org/packages/netkod-bilisim/tr-tax-number-verification" rel="nofollow">
+    <img src="https://img.shields.io/packagist/dt/netkod-bilisim/tr-tax-number-verification" alt="Total Downloads">
+</a>
+
+<a href="https://packagist.org/packages/netkod-bilisim/tr-tax-number-verification" rel="nofollow">
+    <img src="https://poser.pugx.org/netkod-bilisim/tr-tax-number-verification/dependents.svg" alt="Dependents">
+</a>
+
+<a href="https://packagist.org/packages/netkod-bilisim/tr-tax-number-verification" rel="nofollow">
+    <img src="https://img.shields.io/packagist/l/netkod-bilisim/tr-tax-number-verification" alt="License">
+</a>
+</div>
+
+<div style="text-align: center">
+<a href="https://packagist.org/packages/netkod-bilisim/tr-tax-number-verification" rel="nofollow">
+    <img src="http://poser.pugx.org/netkod-bilisim/tr-tax-number-verification/require/php" alt="PHP Version">
+</a>
+<a href="https://scrutinizer-ci.com/g/netkod-bilisim/tr-tax-number-verification/badges/quality-score.png?b=master" rel="nofollow">
+    <img src="https://scrutinizer-ci.com/g/netkod-bilisim/tr-tax-number-verification/badges/quality-score.png?b=master" alt="Scrutinizer">
+</a>
+<a href="https://github.styleci.io/repos/672299895?branch=master">
+    <img src="https://github.styleci.io/repos/672299895/shield?branch=master" alt="StyleCI">
+</a>
+
+</div>
+
+## <img src="public/assets/images/presentation.png" width="25" height="25"> Introduction
+
 It checks the TR Tax Number.
+
+## <img src="public/assets/images/requirement.png" width="25" height="25"> Requirements
+
+- PHP >= 7.4
+
+## <img src="public/assets/images/inbox.png" width="25" height="25"> Install
+
+```bash
+composer require netkod-bilisim/tr-tax-number-verification:"^1"
+```
+
+## <img src="public/assets/images/web-coding.png" width="25" height="25"> Usage
+
+```php
+use NetkodBilisim\TrTaxNumberVerification;
+
+$company_type = 1; // 1: Real Entity, 2: Legal Entity
+$tax_number = '3331970048';
+$tax_office_code = '035267' // Tax office number registered on GIB;
+
+$result = TrTaxNumberVerification::verify($company_type, $tax_number, $tax_office_code);
+var_dump($result);    # Result: object
+
+# If successful
+#
+#   return (object)[
+#       'status' => false,
+#       'data' => (object)[
+#           'title' => 'title',
+#           'tax_no' => 'tax_no',
+#       ]
+#   ];
+
+# If not successful
+#
+#   return (object)[
+#       'status' => false,
+#       'error' => (object)[
+#           'code' => 'code
+#           'description' => 'description'
+#       ]
+#   ];
+```
+
+## <img src="public/assets/images/licensing.png" width="25" height="25"> License
+
+This package is open source software licensed under
+the [MIT License](https://opensource.org/license/mit/).
+
---
+++ src/TrTaxNumberVerification.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace NetkodBilisim;
+
+use GuzzleHttp\Client;
+
+class TrTaxNumberVerification
+{
+    public const API_URL = 'https://ivd.gib.gov.tr/tvd_server';
+    protected static ?Client $client = null;
+    protected static ?string $token = null;
+    protected static array $errorCodeList = [
+        1 => 'İşletme tipi tüzel kişi için 1, şahıs şirketi için 2 olmalıdır!',
+        2 => 'Vergi numarası geçerli değil!',
+        3 => 'Servise ulaşılamıyor!',
+        4 => 'Firma bilgileri bulunamadı!',
+    ];
+
+    public static function verify(int $company_type, string $tax_number, string $tax_office_no)
+    {
+        if (!in_array($company_type, [1, 2])) {
+            return static::errorResponse(1);
+        }
+
+        if (($company_type === 1 && !TrTaxNumberValidation::validate($tax_number)) || ($company_type === 2 && !TrIdentityNumberValidation::validate($tax_number))) {
+            return static::errorResponse(2);
+        }
+
+        $jp = static::makeQuery($company_type, $tax_number, $tax_office_no);
+        $response = static::getClient()->post(static::API_URL . '/dispatch', [
+            'query' => [
+                'cmd' => 'vergiNoIslemleri_vergiNumarasiSorgulama',
+                'token' => static::getToken(),
+                'jp' => json_encode($jp),
+            ]
+        ]);
+        $response = json_decode($response->getBody());
+
+        if (isset($response->error)) {
+            return static::errorResponse(3);
+        } else if (isset($response->data) && !isset($response->data->durum)) {
+            return static::errorResponse(4);
+        }
+
+        return (object)[
+            'status' => true,
+            'data' => (object)[
+                'title' => $response->data->unvan,
+                'tax_no' => $response->data->vkn,
+            ]
+        ];
+    }
+
+    public static function errorResponse(int $errorCode): object
+    {
+        return (object)[
+            'status' => false,
+            'error' => (object)[
+                'code' => $errorCode,
+                'description' => static::$errorCodeList[$errorCode]
+            ]
+        ];
+    }
+
+    public static function makeQuery(int $company_type, string $tax_number, string $tax_office_no): array
+    {
+        $vkn1 = '';
+        $tckn1 = '';
+        if ($company_type === 1) {
+            $vkn1 = $tax_number;
+        } else if ($company_type === 2) {
+            $tckn1 = $tax_number;
+        }
+
+        return [
+            'dogrulama' => [
+                'vkn1' => $vkn1,
+                'tckn1' => $tckn1,
+                'vergidaireleri' => $tax_office_no,
+            ]
+        ];
+    }
+
+    public static function getClient(): Client
+    {
+        if (static::$client !== null) {
+            return static::$client;
+        }
+
+        return static::$client = new Client();
+    }
+
+    public static function getToken(): string
+    {
+        if (static::$token !== null) {
+            return static::$token;
+        }
+
+        $response = static::getClient()->post(static::API_URL . '/assos-login', [
+            'query' => [
+                'assoscmd' => 'cfsession',
+                'fskey' => 'intvrg.fix.session',
+                'fuserid' => 'INTVRG_FIX',
+            ]
+        ]);
+
+        return static::$token = json_decode($response->getBody())->token;
+    }
+}
